cc1dc665d8196cdd5b2cd01d3242cf1850adfdb4
Robson Teixeira <robson.teixeira2000@gmail.com>
2019-10-08 19:51:08 -0300

    Criação do ItemDAO

--- AppPetShop/app/src/main/java/com/example/apppetshop/DAO/ItemDAO.java
+++ AppPetShop/app/src/main/java/com/example/apppetshop/DAO/ItemDAO.java
@@ -1,4 +1,32 @@
 package com.example.apppetshop.DAO;
 
-public class ItemDAO {
+import com.example.apppetshop.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemDAO implements Dao<Item>{
+
+    private List<Item> items = new ArrayList<>();
+
+
+    @Override
+    public Item get(int id) {
+        return items.get(id);
+    }
+
+    @Override
+    public List<Item> getAll() {
+        return items;
+    }
+
+    @Override
+    public void save(Item item) {
+        items.add(item);
+    }
+
+    @Override
+    public void delete(Item item) {
+        items.remove(item);
+    }
 }
