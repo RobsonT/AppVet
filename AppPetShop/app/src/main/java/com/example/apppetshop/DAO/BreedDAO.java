d30dac4ddc7ee3968c1c3ebf5a9f1b9dcb09f22e
Robson Teixeira <robson.teixeira2000@gmail.com>
2019-10-08 21:33:15 -0300

    Criação do RacaDAO

--- AppPetShop/app/src/main/java/com/example/apppetshop/DAO/BreedDAO.java
+++ AppPetShop/app/src/main/java/com/example/apppetshop/DAO/BreedDAO.java
@@ -1,4 +1,31 @@
 package com.example.apppetshop.DAO;
 
-public class BreedDAO {
+import com.example.apppetshop.model.Raca;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BreedDAO implements Dao<Raca>{
+
+    private List<Raca> breeds = new ArrayList<>();
+
+    @Override
+    public Raca get(int id) {
+        return breeds.get(id);
+    }
+
+    @Override
+    public List<Raca> getAll() {
+        return breeds;
+    }
+
+    @Override
+    public void save(Raca breed) {
+        breeds.add(breed);
+    }
+
+    @Override
+    public void delete(Raca breed) {
+        breeds.remove(breed);
+    }
 }
