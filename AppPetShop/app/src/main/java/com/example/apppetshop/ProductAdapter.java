5596b848e7546f4784640db5ee83b09e1ad0e960
Robson Teixeira <robson.teixeira2000@gmail.com>
2019-11-05 14:04:07 -0300

    teste favoritos

--- AppPetShop/app/src/main/java/com/example/apppetshop/ProductAdapter.java
+++ AppPetShop/app/src/main/java/com/example/apppetshop/ProductAdapter.java
@@ -53,25 +53,12 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
         holder.textProduct.setText(product.getNome());
         holder.imgProduct.setImageResource(product.getImagem());
         holder.textPreco.setText("R$" + String.valueOf(product.getPreco()));
+
         for(Favorito f: favoritos){
             if(f.getIdProduto() == product.getId()){
                 holder.favoriteLeft.setImageResource(R.drawable.ic_favorite_black_24dp);
                 holder.favoriteLeft.setTag("true");
             }
-        }
-        if (position < productList2.size()) {
-            product2 = productList2.get(position);
-            holder.textProduct1.setText(product2.getNome());
-            holder.imgProduct1.setImageResource(product2.getImagem());
-            holder.textPreco1.setText("R$" + String.valueOf(product2.getPreco()));
-            for(Favorito f: favoritos){
-                if(f.getIdProduto() == product2.getId()){
-                    holder.favoriteRight.setImageResource(R.drawable.ic_favorite_black_24dp);
-                    holder.favoriteRight.setTag("true");
-                }
-            }
-        }else{
-            holder.secondColumn.setVisibility(View.INVISIBLE);
         }
 
         holder.favoriteLeft.setOnClickListener(new View.OnClickListener(){
@@ -80,38 +67,16 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
                 if(holder.favoriteLeft.getTag().equals("false")) {
                     holder.favoriteLeft.setImageResource(R.drawable.ic_favorite_black_24dp);
                     holder.favoriteLeft.setTag("true");
-                    favorito.setIdProduto(productList1.get(position).getId());
+                    favorito.setIdProduto(product.getId());
                     favorito.setIdCliente(clientId);
                     Log.v("Id2", String.valueOf(favorito.getIdProduto()));
                     favoritoDao.save(favorito);
                 }else{
                     holder.favoriteLeft.setImageResource(R.drawable.ic_favorite);
-                    favorito.setIdProduto(product2.getId());
+                    favorito.setIdProduto(product.getId());
                     favorito.setIdCliente(clientId);
                     favoritoDao.delete(favorito);
                     holder.favoriteLeft.setTag("false");
-                }
-            }
-        });
-
-        holder.favoriteRight.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                if(holder.favoriteRight.getTag().equals("false")) {
-                    holder.favoriteRight.setImageResource(R.drawable.ic_favorite_black_24dp);
-                    holder.favoriteRight.setTag("true");
-                    favorito.setIdProduto(product2.getId());
-                    favorito.setIdCliente(0);
-                    Log.v("Posição", String.valueOf(position));
-                    Log.v("Id1", String.valueOf(product2.getId()));
-                    Log.v("Id2", String.valueOf(favorito.getIdProduto()));
-                    favoritoDao.save(favorito);
-                }else{
-                    holder.favoriteRight.setImageResource(R.drawable.ic_favorite);
-                    holder.favoriteRight.setTag("false");
-                    favorito.setIdProduto(product2.getId());
-                    favorito.setIdCliente(0);
-                    favoritoDao.delete(favorito);
                 }
             }
         });
@@ -120,17 +85,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
             @Override
             public void onClick(View view) {
                 Intent i = new Intent( view.getContext(), ProdutoDescricao.class);
-                i.putExtra( "idProduto", String.valueOf(productList1.get(position).getId()));
-                i.putExtra("idCliente", String.valueOf(clientId));
-                view.getContext().startActivity(i);
-            }
-        });
-
-        holder.secondColumn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent( view.getContext(), ProdutoDescricao.class);
-                i.putExtra( "idProduto", String.valueOf(productList2.get(position).getId()));
+                i.putExtra( "idProduto", String.valueOf(productList.get(position).getId()));
                 i.putExtra("idCliente", String.valueOf(clientId));
                 view.getContext().startActivity(i);
             }
@@ -140,35 +95,25 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ViewHold
 
     @Override
     public int getItemCount() {
-        return productList1.size();
+        return productList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
         ImageView imgProduct;
         TextView textProduct;
         TextView textPreco;
-        ImageView imgProduct1;
-        TextView textProduct1;
-        TextView textPreco1;
         CardView cv;
-        LinearLayout secondColumn;
         LinearLayout firstColumn;
         ImageView favoriteLeft;
-        ImageView favoriteRight;
 
         public ViewHolder(View itemView) {
             super(itemView);
             imgProduct = itemView.findViewById(R.id.imgProduct);
             textProduct = itemView.findViewById(R.id.nomeProduto);
             textPreco = itemView.findViewById(R.id.precoProduto);
-            imgProduct1 = itemView.findViewById(R.id.imgProduct1);
-            textProduct1 = itemView.findViewById(R.id.nomeProduto1);
-            textPreco1 = itemView.findViewById(R.id.precoProduto1);
             cv = itemView.findViewById(R.id.cardView);
-            secondColumn = itemView.findViewById(R.id.secondColumn);
             firstColumn = itemView.findViewById(R.id.firstColumn);
             favoriteLeft = itemView.findViewById(R.id.ic_favorite1);
-            favoriteRight = itemView.findViewById(R.id.ic_favorite);
         }
     }
 }
